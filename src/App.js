b0532f76c28cf006968de09c071aa3396c6c399d
Damian Mach <mach.damian@gmail.com>
2020-07-25 21:57:27 +0200

    Add youtube iframe embed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-// import "./App.css";
 import styled from "styled-components";
 import logo from "./assets/flag.png";
 import { Fonts } from "./fonts/fonts";
+import { GlobalStyle } from "./styles/global-style";
 
 function App() {
   return (
     <AppWrapper>
+      <GlobalStyle />
       <Fonts />
       <AppHeader>
         <AppLogo src={logo} alt="flag" />
@@ -17,22 +18,32 @@ function App() {
       </AppHeader>
       <CamerasWrapper>
         <Camera>
-          <iframe
-            width="560"
-            title="film1"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/Y1vfnYpOFD8?rel=0&enablejsapi=1"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          <Iframe videoId="1nKLpC8Ebbc" title="Jakubowy na wyjscie" />
         </Camera>
       </CamerasWrapper>
     </AppWrapper>
   );
 }
 
-const Camera = styled.div``;
+const Iframe = ({ videoId = "", title = "Empty" } = {}) => {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      title="test"
+      src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  );
+};
+
+const Camera = styled.div`
+  position: relative;
+  display: block;
+  padding: 10px;
+`;
 
 const CamerasWrapper = styled.div`
   position: relative;
@@ -50,7 +61,9 @@ const AppLogo = styled.img`
   // height: 200px;
   background: #fff;
 `;
-const AppTitle = styled.h2``;
+const AppTitle = styled.h2`
+  font-size: 26px;
+`;
 const AppHeader = styled.div`
   position: relative;
   display: flex;
